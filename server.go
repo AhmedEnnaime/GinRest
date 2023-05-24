0c1cd3f02e514b452512605c123f2015d5e0cf38
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-05-24 09:55:44 +0100

    feat: :tada: starting to work with gin

---
+++ server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/AhmedEnnaime/GinRest/controller"
+	"github.com/AhmedEnnaime/GinRest/service"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService service.VideoService = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(201, videoController.Save(ctx))
+	})
+
+	server.Run(":8080")
+}
